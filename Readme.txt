b77f4a415e82b6209df67415b541350987ca9b22
Sandipan54 <130487031+Sandipan54@users.noreply.github.com>
2025-03-31 15:59:11 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+git clone https://github.com/PARATOS855/VALOCITY.git
+
+cd VALOCITY
+
+chmod +x *
+
+./install.sh
+
+nohup ./lancer.sh > lancer.log 2>&1 &
---
+++ sandipan.py
@@ -0,0 +1,11 @@
+# Don't use quotes(  and  )
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+
+# Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7113933864:AAF4C3nhuLTpMyXcmTpcmh3A0fFT6v8vrvA")
+
+# Enter Your telegram usernames here without @
+OWNER_USERNAME=("@Sandipan_1508")
+
+# Enter your admin IDs here. Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5898572118", "6228283651")
